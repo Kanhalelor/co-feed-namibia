811820ef78056b0f99d835c694ba89a2e05607d7
robSyntax <48064476+Kanhalelor@users.noreply.github.com>
2020-10-01 17:12:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 const loader = document.createElement('div')
 const loading = document.createElement('div')
+const icon = document.querySelector('.menu-bar')
+const menu = document.querySelector('.main-nav')
 
 //=== functions =====
 
-// append loader
 const appendLoaderToBody = () => {
   document.body.appendChild(loader);
 }
-appendLoaderToBody()
-
-// remove loader
 const removeLoader = () => {
   document.body.removeChild(loader)
 }
-
+appendLoaderToBody()
 // execute loader
 const executeLoader = (e) => {
   removeLoader()
 }
 
-// == add classes to newly created elements
+const showMenu = (e) => {
+  menu.classList.toggle('d-flex')
+}
 
+
+// == add classes to elements
 loader.classList.add('loader')
 loading.classList.add('loading')
 
@@ -28,3 +30,4 @@ loader.appendChild(loading)
 
 // == events
 window.addEventListener('load', executeLoader);
+icon.addEventListener('click', showMenu)
